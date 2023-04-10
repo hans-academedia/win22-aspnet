37f92e879ea053197d271c6d4c40d2b0fee6ae6e
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-04-10 09:22:51 +0200

    lektion 4 (Identity - Individual Accounts)

---
+++ lektion-4/Authentications/03_IndividualAccounts/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _03_IndividualAccounts.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _03_IndividualAccounts.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ lektion-4/Authentications/03_IndividualAccounts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _03_IndividualAccounts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
